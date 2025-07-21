6d2fd470ebc093f86fa9731fb389db0ba3d17e37
Anupam Katiyar <anupamkatiyar934@gmaile.com>
2025-07-21 16:02:40 +0530

    Fix: homepage route

--- port/views.py
+++ port/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def portfolio(request):
-    return render(request,'port/portfolio.html')
+    return HttpResponse(request,'port/portfolio.html')
